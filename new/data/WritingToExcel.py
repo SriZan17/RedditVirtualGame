5c2eb07ad67d9f06468a47c37a3200a3bca099c9
SriZan17 <68002674+SriZan17@users.noreply.github.com>
2021-02-07 16:30:59 +0545

    Update WritingToExcel.py
    kmk

--- new/data/WritingToExcel.py
+++ new/data/WritingToExcel.py
@@ -5,7 +5,7 @@ import openpyxl
 todays_price = requests.get("https://nepse-data-api.herokuapp.com/data/todaysprice")
 data = todays_price.json()
 
-workbook = openpyxl.load_workbook("./data/STONKS V2.5Beta.xlsx")
+workbook = openpyxl.load_workbook("STONKS V2.5Beta.xlsx")
 sheet = workbook.get_sheet_by_name("STONKS")
 
 for i in range(len(data)):
@@ -17,7 +17,6 @@ for i in range(len(data)):
         a = sheet["A"+str(j)]
         c = sheet["C"+str(j)]
         if a.value == company:
-            # print (a.value) 
-            # print (c.value,price)
             c.value =  price
-workbook.save("./data/STONKS V2.5Beta.xlsx")
+            
+workbook.save("STONKS V2.5Beta.xlsx")
